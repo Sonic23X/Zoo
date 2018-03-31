38b2ba93efa9ca33e73361ba16d84d59e6464efb
Alfredo Ortega <omar.alfredo49@gmail.com>
2018-03-31 16:22:05 -0600

    0.3

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
+
+    <script type="text/javascript" src="bootstrap/js/jquery.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/popper.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="font/fontawesome-all.min.js"></script>
+
+    <title>Zoologico</title>
+  </head>
+  <body>
+    <center>
+      <p class="title">Zoologico</p>
+    </center>
+    <br>
+
+    <div class="target">
+
+        <div class="icon">
+          <span class="fas fa-paw"></span>
+        </div>
+
+        <div class="info">
+          <center>
+            <p class="title2">Animales</p>
+          </center>
+          <input type="button" class="btn btn-primary form-control" value="Consultar">
+
+        </div>
+
+    </div>
+
+    <div class="target">
+
+        <div class="icon">
+          <span class="fas fa-user-md"></span>
+        </div>
+
+        <div class="info">
+          <center>
+            <p class="title2">Cuidadores</p>
+          </center>
+          <input type="button" class="btn btn-primary form-control" value="Consultar">
+
+        </div>
+
+    </div>
+
+    <div class="target">
+
+        <div class="icon">
+          <span class="fas fa-home"></span>
+        </div>
+
+        <div class="info">
+          <center>
+            <p class="title2">Habitads</p>
+          </center>
+          <input type="button" class="btn btn-primary form-control" value="Consultar">
+
+        </div>
+
+    </div>
+
+    <div class="target">
+
+        <div class="icon">
+          <span class="fas fa-medkit"></span>
+        </div>
+
+        <div class="info">
+          <center>
+            <p class="title2">Veterinarios</p>
+          </center>
+          <input type="button" class="btn btn-primary form-control" value="Consultar">
+
+        </div>
+
+    </div>
+
+    <div class="target">
+
+        <div class="icon">
+          <span class="fas fa-stethoscope"></span>
+        </div>
+
+        <div class="info">
+          <center>
+            <p class="title2">Revisiones</p>
+          </center>
+          <input type="button" class="btn btn-primary form-control" value="Consultar">
+
+        </div>
+
+    </div>
+
+  </body>
+</html>
